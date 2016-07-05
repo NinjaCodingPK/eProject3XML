24ebd32461762e45d36c77aac4a5b321015e0564
wookie <kononenkoa995@ukr.net>
2016-07-05 17:23:43 +0300

    Remastered.

--- src/epam3xml/Epam3XML.java
+++ src/epam3xml/Epam3XML.java
@@ -6,35 +6,75 @@
 package epam3xml;
 
 import generated.Greenhouse;
+import java.io.File;
 import model.DomParser;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import model.SaxParser;
+import model.StaxParser;
+import model.Xpath;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author wookie
  */
 public class Epam3XML {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        DomParser parser = new DomParser();
-        
+    private static void runParsers(String fileName){
         try {
-            parser.parse(new FileInputStream("./src/xml/greenhouse"));
+            new DomParser().parse( new FileInputStream(fileName) );
+            new SaxParser().parse( new FileInputStream(fileName) );
         } catch (Exception ex) {
-            Logger.getLogger(Epam3XML.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(XML2015.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    
+    public static void main(String[] args) 
+            throws JAXBException, SAXException, ParserConfigurationException, IOException, XPathExpressionException, Exception {
+        String filename = "./src/xml/greenhouse.xml";
         
-        Greenhouse greenhouse = parser.getGreenhouse();
+        System.out.println("DOM");
+        DomParser dom = new DomParser();
+        dom.parse(new FileInputStream(filename));
+        Greenhouse greenhouse = dom.getGreenhouse();
+        System.out.println(greenhouse.getFlower().get(0).getGrowingTips().getLighting());
         
-        System.out.println(greenhouse.getFlower().get(0).getOrigin());
+        System.out.println("SAX");
+        SaxParser sax = new SaxParser();
+        sax.parse(new FileInputStream(filename));
+        greenhouse = sax.getGreenhouse();
+        System.out.println(greenhouse.getFlower().get(0).getName());
+
+        System.out.println("STAX");
+        StaxParser stax = new StaxParser();
+        stax.parse(new FileInputStream(filename));
+        greenhouse = stax.getGreenhouse();
+        System.out.println(greenhouse.getFlower().get(1).getName());   
+        System.out.println(greenhouse.getFlower().get(0).getGrowingTips().getLighting());
+            
+        
+//        runParsers(filename);
+//        JAXBContext context = JAXBContext.newInstance( Greenhouse.class );
+//        Unmarshaller unm = context.createUnmarshaller();
+//        Greenhouse greenhouse = (Greenhouse) unm.unmarshal(new File(filename));
+//        
+//        System.out.println( greenhouse.getFlower().get(1).getName() );
+//        
+//        Xpath xpath = new Xpath();
+//        xpath.initParser(filename);
+//        System.out.println("XPATH:" + xpath.getFlowerName(1));
+//        System.out.println("---------------------------------");
+    
     }
     
    
